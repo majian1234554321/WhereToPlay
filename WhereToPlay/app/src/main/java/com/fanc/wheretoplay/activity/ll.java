c4a5ffaa59b59f69714b6480be093d0a65e320c7
xuxiaoyang <2802214140@qq.com>
2017-11-09 14:14:03 +0800

    test

---
+++ WhereToPlay/app/src/main/java/com/fanc/wheretoplay/activity/ll.java
@@ -0,0 +1,9 @@
+package com.fanc.wheretoplay.activity;
+
+/**
+ * Created by peace on 2017/11/9.
+ */
+
+public class ll {
+    int g = 0 ;
+}
---
+++ WhereToPlay/app/src/main/java/com/fanc/wheretoplay/fragment/MineFriendsFragment.java
@@ -0,0 +1,11 @@
+package com.fanc.wheretoplay.fragment;
+
+import com.fanc.wheretoplay.base.BaseFragment;
+
+/**
+ * Created by peace on 2017/11/9.
+ */
+
+public class MineFriendsFragment extends BaseFragment {
+    int a = 4;
+}
