6181441e64458ee7e92fcd594ffb3a2ea90eddc3
xuxiaoyang <2802214140@qq.com>
2017-11-10 10:46:40 +0800

    test

--- WhereToPlay/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="38e4c6f7-d7c5-4ed9-8874-047cccbf7ca2")
-public class DataBindingInfo {}
