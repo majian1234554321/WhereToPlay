9c0117434560bc7f9e65d796424d39366aae704c
admin <99571202@qq.com>
2017-11-17 10:07:36 +0800

    合并提交

---
+++ WhereToPlay/app/src/main/java/com/fanc/wheretoplay/activity/EvaluationSuccessActivity.kt
@@ -0,0 +1,40 @@
+package com.fanc.wheretoplay.activity
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import com.fanc.wheretoplay.R
+import com.fanc.wheretoplay.adapter.EvaluationSuccessAdapter
+import com.fanc.wheretoplay.base.BaseActivity
+
+import kotlinx.android.synthetic.main.activity_evaluation_success.*
+
+class EvaluationSuccessActivity : BaseActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.menu_iv_lefticon -> {
+                finish()
+            }
+            R.id.tv_back -> {
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var list = ArrayList<String>()
+        list.add("'1")
+        list.add("'2")
+        list.add("'3")
+        setContentView(R.layout.activity_evaluation_success)
+        tv_title.text = "评价成功"
+
+        val evaluationSuccessAdapter = EvaluationSuccessAdapter(list)
+        recycle.adapter = evaluationSuccessAdapter
+        recycle.layoutManager = LinearLayoutManager(this)
+        menu_iv_lefticon.setOnClickListener(this)
+
+    }
+
+
+}
