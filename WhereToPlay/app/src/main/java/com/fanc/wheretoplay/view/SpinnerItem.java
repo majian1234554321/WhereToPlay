df331bcf16251909759418a7c0f761925a3a64b7
admin <99571202@qq.com>
2018-03-21 17:44:21 +0800

    修改分享鏈接

---
+++ WhereToPlay/app/src/main/java/com/fanc/wheretoplay/view/SpinnerItem.java
@@ -0,0 +1,52 @@
+package com.fanc.wheretoplay.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import com.fanc.wheretoplay.R;
+
+import java.util.List;
+
+/**
+ * @author admin
+ * @date 2018/3/21
+ */
+
+public class SpinnerItem extends RelativeLayout {
+
+    private Spinner spinner;
+
+    public SpinnerItem(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SpinnerItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public SpinnerItem(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public void init(Context context) {
+        View view = View.inflate(context, R.layout.spinneritem, this);
+        spinner = view.findViewById(R.id.sp);
+    }
+
+    public void setData(List<String> list, Context context) {
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item, list);
+
+        spinner.setAdapter(arrayAdapter);
+    }
+
+
+}
